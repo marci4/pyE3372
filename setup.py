8fd191c4f342733d84f020e4efd6bd67de84466a
marci4 <admin@marci4.de>
2021-06-19 12:44:36 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyE3372',
+    version='',
+    packages=['tests'],
+    url='https://github.com/marci4/pyE3372',
+    license='',
+    author='marci4',
+    author_email='admin@marci4.de',
+    description='Wrapper for Huawei E3372'
+)
